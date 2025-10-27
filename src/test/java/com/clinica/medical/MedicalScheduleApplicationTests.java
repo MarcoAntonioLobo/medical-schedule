e8f5c5583d286440b1d20f00561372ec9f3806f5
MarcoAntonioLobo <marcoantoniolobo82@gmail.com>
2025-10-27 21:35:43 +0000

    Project created

---
+++ src/test/java/com/clinica/medical/MedicalScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.medical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
